0cba276e160cc38ebfe4928daf02fe73b2e43b0d
FrancoHub25 <francoretamozo25@gmail.com>
2022-07-21 02:07:43 -0500

    fdd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrudDocenteComponent } from './components/crud-docente/crud-docente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudDocenteComponent
   ],
   imports: [
     BrowserModule
